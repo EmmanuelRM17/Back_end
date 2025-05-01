e50b032de3733bb10a0239df7ba3dd09180197c9
EmmanuelRM17 <emma041117@gmail.com>
2025-05-01 12:51:32 -0600

    new

--- index.js
+++ index.js
@@ -46,7 +46,7 @@ app.use(express.json());
 
 // Tus rutas existentes
 const userRoutes = require("./routes/userRoutes");
-const chatbot = require("./routes/chatbot.js");
+const chatbot = require("./routes/chatbot");
 const Registrer = require("./routes/registrer");
 const Citas = require("./routes/admin/citas")
 const Tratamientos = require("./routes/admin/tratamientos.js")
