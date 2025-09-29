b51184a7586621bb3333f0e5dc3a64cbb147fcf2
Emmanuel Rodríguez <emma041117@gmail.com>
2025-09-29 02:28:27 -0600

    new

--- routes/registrer.js
+++ routes/registrer.js
@@ -23,7 +23,7 @@ const transporter = nodemailer.createTransport({
   requireTLS: true, // Fuerza el uso de TLS para seguridad
   auth: {
     user: "sistema@odontologiacarol.com",
-    pass: "ql?5Zn4^2U",
+    pass: "sP8+?;Vs:",
   },
   connectionTimeout: 60000, // 60 segundos de tiempo de espera
   greetingTimeout: 30000, // 30 segundos para el saludo del servidor
